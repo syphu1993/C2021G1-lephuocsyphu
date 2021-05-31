e2bb87eb74b605d6c9cb93e0b88bec07cd1823b7
syphu1993 <phusylee@gmail.com>
2021-05-31 20:56:36 +0700

    updateJdbc

---
+++ MODULE 3/12. JDBC & CRUD/Practice&Exercise/Demo/src/model/repository/UserRepository.java
@@ -0,0 +1,151 @@
+package model.repository;
+
+import model.bean.User;
+
+import javax.jws.soap.SOAPBinding;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRepository {
+    BaseRepository baseRepository = new BaseRepository();
+    private static final String INSERT_USERS_SQL = "INSERT INTO users" + "  (name, email, country) VALUES " +
+            " (?, ?, ?);";
+
+    private static final String SELECT_USER_BY_ID = "select id,name,email,country from users where id =?";
+    private static final String SELECT_ALL_USERS = "select * from users";
+    private static final String DELETE_USERS_SQL = "delete from users where id = ?;";
+    private static final String UPDATE_USERS_SQL = "update users set name = ?,email= ?, country =? where id = ?;";
+    private static final String SELECT_USER_BY_COUNTRY="select id,name,email,country from users where country=?";
+    private static final String SELECT_ALL_USERS_BY_NAME = "select * from users order by `name`";
+
+    public List<User> selectAllUsers() {
+        Connection connection = baseRepository.connectDataBase();
+        List<User> userList = new ArrayList<>();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_USERS);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) { // duyệt trên từng hàng của bảng
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String email = resultSet.getString("email");
+                String country = resultSet.getString("country");
+                User user = new User(id, name, email, country);
+                userList.add(user);
+            }
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userList;
+    }
+    public User selectUser(int id){
+        User user  = null;
+        Connection connection =baseRepository.connectDataBase();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_ID);
+            preparedStatement.setInt(1,id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                int id1 = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String email = resultSet.getString("email");
+                String country = resultSet.getString("country");
+                user = new User(id1, name, email, country);
+            }
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+    public boolean updateUser(User user){
+        Connection connection =baseRepository.connectDataBase();
+        boolean check=false;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_USERS_SQL);
+            preparedStatement.setString(1,user.getName());
+            preparedStatement.setString(2,user.getEmail());
+            preparedStatement.setString(3,user.getCountry());
+            preparedStatement.setInt(4,user.getId());
+            check =preparedStatement.executeUpdate()>0;
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return check;
+    }
+    public void insertUser(User user){
+        Connection connection =baseRepository.connectDataBase();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL);
+            preparedStatement.setString(1,user.getName());
+            preparedStatement.setString(2,user.getEmail());
+            preparedStatement.setString(3,user.getCountry());
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public boolean deleteUser(int id){
+        Connection connection =baseRepository.connectDataBase();
+        boolean check=false;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_USERS_SQL);
+            preparedStatement.setInt(1,id);
+            check =preparedStatement.executeUpdate()>0;
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return check;
+    }
+    public List<User> findByCountry(String country) {
+        Connection connection =baseRepository.connectDataBase();
+        List<User> users = new ArrayList<>();
+        User user = null;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_COUNTRY);
+            preparedStatement.setString(1,country);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                int id=rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country1 = rs.getString("country");
+                user = new User(id, name, email, country1);
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.getMessage();
+        }
+        return users;
+    }
+    public List<User> sortByName() {
+        Connection connection =baseRepository.connectDataBase();
+        List<User> users = new ArrayList<>();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_USERS_BY_NAME);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                users.add(new User(id, name, email, country));
+            }
+        } catch (SQLException e) {
+            e.getMessage();
+        }
+        return users;
+    }
+}
