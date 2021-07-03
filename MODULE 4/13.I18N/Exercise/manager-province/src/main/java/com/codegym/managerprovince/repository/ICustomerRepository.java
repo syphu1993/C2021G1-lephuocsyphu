58114efc5032309266b2ebf283c0456d09b74b51
syphu1993 <phusylee@gmail.com>
2021-07-03 11:20:39 +0700

    i18n

---
+++ MODULE 4/13.I18N/Exercise/manager-province/src/main/java/com/codegym/managerprovince/repository/ICustomerRepository.java
@@ -0,0 +1,12 @@
+package com.codegym.managerprovince.repository;
+
+
+import com.codegym.managerprovince.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer,Long> {
+}
---
+++ MODULE 4/13.I18N/Exercise/manager-province/src/main/java/com/codegym/managerprovince/service/IProvinceService.java
@@ -0,0 +1,7 @@
+package com.codegym.managerprovince.service;
+
+
+import com.codegym.managerprovince.model.Province;
+
+public interface IProvinceService extends IGeneralService<Province> {
+}
